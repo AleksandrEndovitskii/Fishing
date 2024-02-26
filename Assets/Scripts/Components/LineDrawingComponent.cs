3cba2336a418c82c043eafccfe532c1b69a1e716
Aleksandr Endovitskii <endovitsky.a.p@gmail.com>
2024-02-26 16:17:32 +0100

    Add drawing line to cursor

---
+++ Assets/Scripts/Components/LineDrawingComponent.cs
@@ -0,0 +1,43 @@
+using System.Collections.Generic;
+using UnityEngine;
+
+namespace Components
+{
+    [RequireComponent(typeof(LineRenderer))]
+    public class LineDrawingComponent : MonoBehaviour
+    {
+        private LineRenderer _lineRenderer;
+
+        private Camera _camera;
+
+        private void Start()
+        {
+            _lineRenderer = GetComponent<LineRenderer>();
+
+            _camera = Camera.main;
+
+            _lineRenderer.startWidth = 0.01f;
+            _lineRenderer.endWidth = 0.01f;
+            _lineRenderer.positionCount = 2;
+        }
+
+        private void Update()
+        {
+            DrawLine(Vector3.zero, Input.mousePosition);
+        }
+
+        private void DrawLine(params Vector3[] screenPositions)
+        {
+            var worldPositions = new List<Vector3>();
+            foreach (var screenPosition in screenPositions)
+            {
+                var position = new Vector3(screenPosition.x, screenPosition.y, _camera.nearClipPlane);
+                var worldPosition = _camera.ScreenToWorldPoint(position);
+                worldPositions.Add(worldPosition);
+            }
+
+            _lineRenderer.positionCount = worldPositions.Count;
+            _lineRenderer.SetPositions(worldPositions.ToArray());
+        }
+    }
+}
