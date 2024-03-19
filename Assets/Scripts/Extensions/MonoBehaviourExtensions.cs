1047b0d4ccf678b4182ce62290d50db6660c372f
Aleksandr Endovitskii <endovitsky.a.p@gmail.com>
2024-03-19 08:08:38 +0100

    Add MonoBehaviourExtensions

---
+++ Assets/Scripts/Extensions/MonoBehaviourExtensions.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using UnityEngine;
+
+namespace Extensions
+{
+    public static class MonoBehaviourExtensions
+    {
+        public static void InvokeActionPeriodically(this MonoBehaviour monoBehaviour,
+            Action action, float intervalSeconds = 1f)
+        {
+            var invokeActionPeriodicallyCoroutine =
+                monoBehaviour.StartCoroutine(InvokeActionPeriodicallyCoroutine(action, intervalSeconds));
+        }
+
+        private static IEnumerator InvokeActionPeriodicallyCoroutine(Action action, float intervalSeconds)
+        {
+            while (true)
+            {
+                action?.Invoke();
+
+                yield return new WaitForSeconds(intervalSeconds); // WaitForSecondsRealtime
+            }
+        }
+    }
+}
