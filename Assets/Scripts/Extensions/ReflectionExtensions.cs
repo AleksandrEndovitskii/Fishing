7114036e1dd4be81846b9fae3eb240991a826e9f
Aleksandr Endovitskii <endovitsky.a.p@gmail.com>
2024-03-19 08:08:26 +0100

    Add ReflectionExtensions

---
+++ Assets/Scripts/Extensions/ReflectionExtensions.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+using Newtonsoft.Json;
+
+namespace Extensions
+{
+    public static class ReflectionExtensions
+    {
+        public static bool ValueEquals<T>(this List<T> list1, List<T> list2)
+        {
+            // TODO: temp solution
+            var list1Json = JsonConvert.SerializeObject(list1);
+            var list2Json = JsonConvert.SerializeObject(list2);
+            var result = list1Json.Equals(list2Json);
+
+            return result;
+        }
+    }
+}
