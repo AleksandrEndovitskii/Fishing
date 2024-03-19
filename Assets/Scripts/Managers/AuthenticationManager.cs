8a43aff03f02a2672e9c6f76bab1854a6f7dcafb
Aleksandr Endovitskii <endovitsky.a.p@gmail.com>
2024-03-19 07:15:13 +0100

    Add functional for monitoring of authorized state of player

--- Assets/Scripts/Managers/AuthenticationManager.cs
+++ Assets/Scripts/Managers/AuthenticationManager.cs
@@ -1,3 +1,4 @@
+using System;
 using Cysharp.Threading.Tasks;
 using Helpers;
 using Unity.Services.Authentication;
@@ -8,6 +9,30 @@ namespace Managers
 {
     public class AuthenticationManager : BaseManager<AuthenticationManager>
     {
+        public event Action<bool> IsAuthorizedChanged = delegate { };
+        public bool IsAuthorized
+        {
+            get => _isAuthorized;
+            set
+            {
+                if (_isAuthorized == value)
+                {
+                    return;
+                }
+
+                if (Debug.isDebugBuild)
+                {
+                    Debug.Log($"{this.GetType().Name}.{ReflectionHelper.GetCallerMemberName()}" +
+                              $"\n{_isAuthorized} -> {value}");
+                }
+
+                _isAuthorized = value;
+
+                IsAuthorizedChanged?.Invoke(_isAuthorized);
+            }
+        }
+        private bool _isAuthorized;
+
         protected override async UniTask Initialize()
         {
             // TODO: move to correct place
@@ -21,11 +46,27 @@ namespace Managers
         {
             IsInitialized = false;
         }
+
         protected override async UniTask Subscribe()
         {
+            await UniTask.WaitUntil(() => UnityServices.State == ServicesInitializationState.Initialized);
+            await UniTask.WaitUntil(() => AuthenticationService.Instance != null);
+
+            AuthenticationService.Instance.SignedIn += AuthenticationService_SignedIn;
+            AuthenticationService.Instance.SignedOut += AuthenticationService_SignedOut;
+            AuthenticationService.Instance.SignInFailed += AuthenticationService_SignInFailed;
+            AuthenticationService.Instance.Expired += AuthenticationService_Expired;
+            UpdateDataInReactiveProperties();
         }
         protected override async UniTask UnSubscribe()
         {
+            if (AuthenticationService.Instance != null)
+            {
+                AuthenticationService.Instance.SignedIn -= AuthenticationService_SignedIn;
+                AuthenticationService.Instance.SignedOut -= AuthenticationService_SignedOut;
+                AuthenticationService.Instance.SignInFailed -= AuthenticationService_SignInFailed;
+                AuthenticationService.Instance.Expired -= AuthenticationService_Expired;
+            }
         }
 
         private async UniTask SignInAnonymouslyAsync()
@@ -45,9 +86,41 @@ namespace Managers
             }
             catch (RequestFailedException requestFailedException)
             {
-                Debug.LogError($"{GetType().Name}.{ReflectionHelper.GetCallerMemberName()}" +
+                Debug.LogError($"{GetType().Name}.{ReflectionHelper.GetCallerMemberName()}_Failed" +
                                      $"\n{nameof(requestFailedException)} == {requestFailedException}");
             }
+        }
+
+        // this method is used because AuthenticationService.Instance do not have reactive properties or logs
+        private void UpdateDataInReactiveProperties()
+        {
+            IsAuthorized = AuthenticationService.Instance.IsAuthorized;
+        }
+
+        private void AuthenticationService_SignedIn()
+        {
+            Debug.Log($"{GetType().Name}.{ReflectionHelper.GetCallerMemberName()}");
+
+            UpdateDataInReactiveProperties();
+        }
+        private void AuthenticationService_SignedOut()
+        {
+            Debug.Log($"{GetType().Name}.{ReflectionHelper.GetCallerMemberName()}");
+
+            UpdateDataInReactiveProperties();
+        }
+        private void AuthenticationService_SignInFailed(RequestFailedException requestFailedException)
+        {
+            Debug.LogError($"{GetType().Name}.{ReflectionHelper.GetCallerMemberName()}" +
+                                 $"\n{nameof(requestFailedException)} == {requestFailedException}");
+
+            UpdateDataInReactiveProperties();
+        }
+        private void AuthenticationService_Expired()
+        {
+            Debug.Log($"{GetType().Name}.{ReflectionHelper.GetCallerMemberName()}");
+
+            UpdateDataInReactiveProperties();
         }
     }
 }
