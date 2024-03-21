026e9a01520c679f07328b4e6f764e4a484396b0
Aleksandr Endovitskii <endovitsky.a.p@gmail.com>
2024-03-21 04:26:14 +0100

    Refactor PlayerModel

--- Assets/Scripts/Models/PlayerModel.cs
+++ Assets/Scripts/Models/PlayerModel.cs
@@ -1,11 +1,33 @@
+using System;
+using Helpers;
+using UnityEngine;
+
 namespace Models
 {
     public class PlayerModel : IModel
     {
+        public event Action<ulong> OwnerClientIdChanged = delegate { };
         public ulong OwnerClientId
         {
-            get;
-            private set;
+            get => _ownerClientId;
+            set
+            {
+                if (_ownerClientId == value)
+                {
+                    return;
+                }
+
+                if (Debug.isDebugBuild)
+                {
+                    Debug.Log($"{this.GetType().Name}.{ReflectionHelper.GetCallerMemberName()}" +
+                              $"\n{_ownerClientId} -> {value}");
+                }
+
+                _ownerClientId = value;
+
+                OwnerClientIdChanged?.Invoke(_ownerClientId);
+            }
         }
+        private ulong _ownerClientId;
     }
 }
