66d1aab047f8539d7b4e03cf47ec17fdb6cd8057
Aleksandr Endovitskii <endovitsky.a.p@gmail.com>
2024-03-21 04:42:06 +0100

    Add PositionMonitoringComponent

---
+++ Assets/Scripts/Components/CharacterComponents/PositionMonitoringComponent.cs
@@ -0,0 +1,42 @@
+﻿using Components.BaseComponents;
+using Cysharp.Threading.Tasks;
+using Extensions;
+using UnityEngine;
+using Views;
+
+namespace Components.CharacterComponents
+{
+    [RequireComponent(typeof(PlayerView))]
+    public class PositionMonitoringComponent : BaseComponent
+    {
+        private PlayerView _playerView;
+
+        protected override async UniTask Initialize()
+        {
+            _playerView = this.gameObject.GetComponent<PlayerView>();
+
+            await UniTask.WaitUntil(() => _playerView != null &&
+                                          _playerView.Model != null);
+
+            this.gameObject.transform.position = _playerView.Model.Position.ToUnity();
+
+            this.InvokeActionPeriodically(() =>
+                {
+                    _playerView.Model.Position = this.gameObject.transform.position.ToSystemNumeric();
+                },
+                0.1f);
+        }
+
+        protected override async UniTask UnInitialize()
+        {
+        }
+
+        protected override async UniTask Subscribe()
+        {
+        }
+
+        protected override async UniTask UnSubscribe()
+        {
+        }
+    }
+}
