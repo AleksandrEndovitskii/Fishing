e6f2dab40ffab279d64e8c12563131a49e7145c9
Aleksandr Endovitskii <endovitsky.a.p@gmail.com>
2024-03-21 04:41:59 +0100

    Add Vector3Extensions

---
+++ Assets/Scripts/Extensions/Vector3Extensions.cs
@@ -0,0 +1,18 @@
+﻿namespace Extensions
+{
+    public static class Vector3Extensions
+    {
+        // Convert UnityEngine.Vector3 to System.Numerics.Vector3
+        public static System.Numerics.Vector3 ToSystemNumeric(this UnityEngine.Vector3 vector)
+        {
+            return new System.Numerics.Vector3(vector.x, vector.y, vector.z);
+        }
+
+        // Convert System.Numerics.Vector3 to UnityEngine.Vector3
+        public static UnityEngine.Vector3 ToUnity(this System.Numerics.Vector3 vector)
+        {
+            return new UnityEngine.Vector3(vector.X, vector.Y, vector.Z);
+        }
+    }
+
+}
