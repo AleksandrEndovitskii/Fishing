5bc4202ed4d8f840b205581613967cca3d6973ea
Aleksandr Endovitskii <endovitsky.a.p@gmail.com>
2024-03-21 02:38:26 +0100

    Add CharactersManager

---
+++ Assets/Scripts/Managers/CharactersManager.cs
@@ -0,0 +1,22 @@
+﻿using Cysharp.Threading.Tasks;
+
+namespace Managers
+{
+    public class CharactersManager : BaseManager<CharactersManager>
+    {
+        protected override async UniTask Initialize()
+        {
+            IsInitialized = true;
+        }
+        protected override async UniTask UnInitialize()
+        {
+            IsInitialized = false;
+        }
+        protected override async UniTask Subscribe()
+        {
+        }
+        protected override async UniTask UnSubscribe()
+        {
+        }
+    }
+}
