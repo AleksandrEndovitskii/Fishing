a595b3eeb82d8f01fb396cb327f1f57ac4c6de20
Aleksandr Endovitskii <endovitsky.a.p@gmail.com>
2024-03-21 04:07:41 +0100

    Refactor MonoBehaviourExtensions

--- Assets/Scripts/Extensions/MonoBehaviourExtensions.cs
+++ Assets/Scripts/Extensions/MonoBehaviourExtensions.cs
@@ -6,11 +6,27 @@ namespace Extensions
 {
     public static class MonoBehaviourExtensions
     {
+        public static Coroutine InvokeActionAfterDelay(this MonoBehaviour monoBehaviour,
+            Action action, float delaySecondsCount)
+        {
+            var invokeActionAfterDelayCoroutine =
+                monoBehaviour.StartCoroutine(InvokeActionAfterDelayCoroutine(action, delaySecondsCount));
+
+            return invokeActionAfterDelayCoroutine;
+        }
+
         public static void InvokeActionPeriodically(this MonoBehaviour monoBehaviour,
             Action action, float intervalSeconds = 1f)
         {
             var invokeActionPeriodicallyCoroutine =
                 monoBehaviour.StartCoroutine(InvokeActionPeriodicallyCoroutine(action, intervalSeconds));
+        }
+
+        private static IEnumerator InvokeActionAfterDelayCoroutine(Action action, float delaySecondsCount)
+        {
+            yield return new WaitForSecondsRealtime(delaySecondsCount);
+
+            action?.Invoke();
         }
 
         private static IEnumerator InvokeActionPeriodicallyCoroutine(Action action, float intervalSeconds)
